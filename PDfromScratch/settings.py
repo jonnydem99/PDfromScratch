beb03535dfbd43d456a2e733996aecc6411e8b6c
Jonathan Niess <niessj@gmail.com>
2020-09-22 12:56:22 -0500

    urls and basic views

--- PDfromScratch/settings.py
+++ PDfromScratch/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PDfromScratch.apps.PDfromScratchConfig',
 ]
 
 MIDDLEWARE = [
